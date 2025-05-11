d039c57b36d81b813fc7c92c46470b41df26b2ac
Andy Nguyen <iam.andynguwin@gmail.com>
2025-05-11 14:02:09 -0700

    New route

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './App.css';
 import ChatBox from './ChatBox';
 import RoleplayNotes from './RoleplayNotes';
+import LoreNotes from '.LoreNotes';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 import axios from 'axios'
@@ -12,6 +13,7 @@ function App() {
     <Router>
       <Routes>
         <Route path='/' element={<RoleplayNotes> </RoleplayNotes>}> </Route>
+        <Route path='lore-build'></Route>
       </Routes>
     </Router>
   )
---
+++ my-app/src/LoreNotes.js
@@ -0,0 +1,20 @@
+import './RoleplayNotes.css';
+import ChatBox from './ChatBox';
+
+function LoreNotes() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <div className="Top">
+          <img alt="Slime" className="image" src="https://gamepedia.cursecdn.com/maplestory2_gamepedia/3/38/Slime.png" />
+        </div>
+        <div className="inputbox"> <ChatBox> </ChatBox> </div>
+        <p><code> Tell me about your character! </code></p>
+        <div className='Top'> </div>
+      </div>
+    </div>
+  );
+    // return <h1> Home page </h1>
+}
+
+export default LoreNotes;
